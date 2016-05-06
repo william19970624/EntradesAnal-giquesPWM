ad53785c9793c7d3eb72dbdb7a296a86ec928a29
william19970624 <19970624gsq@gmail.com>
2016-05-06 19:40:00 +0200

    Morse Guillwermo

---
+++ MorseGuillermo/MorseGuillermo.ino
@@ -0,0 +1,58 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+   //G_guillermo
+    ratlla();
+    ratlla();
+    punt();
+    //espai entre lletres
+    espaiL();
+    //U_guillermo
+    punt();
+    punt();
+    ratlla();
+    //espai entre lletres
+    espaiL();
+    //I_guillermo
+    punt();
+    punt();
+    //espai entre lletres
+    espaiL();
+    //L_guillermo
+    punt();
+    ratlla();
+    punt();
+    punt();
+    //espai entre lletres
+    espaiL();
+    //L_guillermo
+    punt();
+    ratlla();
+    punt();
+    punt();
+    //espai entre lletres
+    espaiL();
+    //E_guillermo
+    punt();
+    //espai entre lletres
+    espaiL();
+    //R_guillermo
+    punt();
+    ratlla();
+    punt();
+    //espai entre lletres
+    espaiL();
+    //M_guillermo
+    ratlla();
+    ratlla();
+    //espai entre lletres
+    espaiL();
+    //O_guillermo
+    ratlla();
+    ratlla();
+    ratlla();
+    
+}
